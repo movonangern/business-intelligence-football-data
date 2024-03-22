14d0aa078032bad4228c182e8b3c37a1eef31ade
Moritz von Angern <moritzvonangern@gmail.com>
2024-03-22 17:38:42 +0100

    streamlit einrichtung

---
+++ streamlit/pages/2_🌍_Spieler.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("Hello")
+st.image("https://img.a.transfermarkt.technology/portrait/header/77-1458201664.jpg?lm=1,https://www.transfermarkt.co.uk/lucio/profil/spieler/77,IT1,Juventus")
